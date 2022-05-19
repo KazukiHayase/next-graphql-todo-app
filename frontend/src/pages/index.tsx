ffdd5036289c264e1e944fdb186e87b1dfc05699
tshibata <t.shibata@buysell-technologies.com>
2022-05-19 19:07:11 +0900

    5/19

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,36 @@
-const IndexPage = () => (
-  <h1>Hello Next.js 👋</h1>
-)
+import React, { useCallback, useEffect, useState } from "react";
+
+const IndexPage = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.isComposing || e.key !== 'Enter') return
+    console.log("test");
+  }
+
+  return (
+    <>
+      <div>一覧</div>
+      <div>
+        作成
+        <input value={todo} onKeyDown={handleKeyDown}/>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>TODO</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* 仮置き */}
+            <td>しなければいけないこと</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
 
 export default IndexPage
